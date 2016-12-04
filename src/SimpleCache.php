d033c7b108dca36e089684f4316832f01781fc9d
artur <arturpanteleev@gmail.com>
2016-12-05 00:34:33 +0300

    правки класса кеширования

--- src/SimpleCache.php
+++ src/SimpleCache.php
@@ -4,16 +4,17 @@ namespace Core;
 
 class SimpleCache implements CacheInterface
 {
-	private $_cacheFolder;
+	protected $cacheFolder = __DIR__ . '/../cache/';
+	protected $ttl = 3600;
 
 	public function __construct()
 	{
-		$this->_cacheFolder = __DIR__  . '/../cache/';
+
 	}
 
 	public function load($key)
 	{
-		$file = $this->_cacheFolder . md5($key);
+		$file = $this->getFilenameFromKey($key);
 		if (file_exists($file))
 		{
 			$data = unserialize(file_get_contents($file));
@@ -24,6 +25,7 @@ class SimpleCache implements CacheInterface
 			else
 			{
 				unlink($file);
+
 				return false;
 			}
 		}
@@ -31,11 +33,15 @@ class SimpleCache implements CacheInterface
 		return false;
 	}
 
-	public function save($key, $data, $time = 60*60*24)
+	public function save($key, $data, $time)
 	{
-		$file = $this->_cacheFolder . md5($key);
+		$file = $this->getFilenameFromKey($key);
 		$content['data'] = $data;
 		$content['time'] = time();
+		if (empty($time))
+		{
+			$time = $this->ttl;
+		}
 		$content['ttl'] = $time;
 		if (file_put_contents($file, serialize($content)))
 		{
@@ -49,10 +55,15 @@ class SimpleCache implements CacheInterface
 
 	public function remove($key)
 	{
-		$file = $this->_cacheFolder . md5($key);
+		$file = $this->getFilenameFromKey($key);
 		if (file_exists($file))
 		{
 			unlink($file);
 		}
+	}
+
+	protected function getFilenameFromKey($key)
+	{
+		return $this->cacheFolder . md5($key);
 	}
 }
